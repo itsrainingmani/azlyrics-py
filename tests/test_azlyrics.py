ce6e32c9bd034d616b2f1f2ab2c47f4cefc4c6dc
Manikandan Sundararajan <tsmanikandan@protonmail.com>
2019-06-04 11:29:10 -0400

    Add more parametric test inputs. Rename azlyics import

--- tests/test_azlyrics.py
+++ tests/test_azlyrics.py
@@ -1,6 +1,6 @@
-from azlyrics import __version__
+from az import __version__
 import pytest
-import azlyrics.azlyrics as az
+import az.lyrics as api
 
 
 def test_version():
@@ -12,11 +12,15 @@ class TestClass(object):
         [
             (("Metallica", "One"), ("metallica", "one")),
             (("The Killers", "The Man"), ("killers", "theman")),
-            (("King Gizzard & the LizArd wizArd", "woRk-This-Time"), ("kinggizzardthelizardwizard", "workthistime"))
+            (("King Gizzard & the LizArd wizArd", "woRk-This-Time"), ("kinggizzardthelizardwizard", "workthistime")),
+            (("King Gizzard &the LizArd wizArd", "Head on / Pill"), ("kinggizzardthelizardwizard", "headonpill")),
+            (("King Gizzard & the LizArd wizArd", "Danger $$$"), ("kinggizzardthelizardwizard", "danger")),
+            (("King Gizzard & the LizArd wizArd", "Her And I (Slow Jam 2)"), ("kinggizzardthelizardwizard", "herandislowjam2")),
+            (("King Gizzard & The Lizard Wizard", "TIme = Fate"), ("kinggizzardthelizardwizard", "timefate")),
         ],
     )
     def test_clean_names(self, inp, exp):
-        assert az.clean_names(inp[0], inp[1]) == exp
+        assert api.clean_names(inp[0], inp[1]) == exp
 
     @pytest.mark.parametrize(
         ("inp", "exp"),
@@ -32,4 +36,20 @@ class TestClass(object):
         ],
     )
     def test_create_url(self, inp, exp):
-        assert az.create_url(inp[0], inp[1]) == exp
+        assert api.create_url(inp[0], inp[1]) == exp
+
+    # @pytest.mark.parametrize(
+    #     ("inp", "exp"),
+    #     [
+    #         (
+    #             ("metallica", "one"),
+    #             ("https://www.azlyrics.com/lyrics/metallica/one.html")
+    #         ),
+    #         (
+    #             ("killers", "theman"),
+    #             ("https://www.azlyrics.com/lyrics/killers/theman.html")
+    #         ),
+    #     ],
+    # )
+    # def test_create_url_uncleaned(self, inp, exp):
+    #     assert api.create_url(inp[0], inp[1]) == exp
